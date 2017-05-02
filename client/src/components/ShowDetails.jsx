7d2266b36c2ec886a900bbebdc38ecc0ccc83153
Robert Ball <robert.ball.1989@gmail.com>
2017-05-02 13:58:17 +0100

    Added more sections to ShowDetails.

--- client/src/components/ShowDetails.jsx
+++ client/src/components/ShowDetails.jsx
@@ -10,19 +10,35 @@ class ShowDetails extends React.Component {
 
     return (
       <div id="film-detail">
+
+        <img src={this.props.film.poster}/>
+
         <h4>
           {this.props.film.show_title}
         </h4>
+
         <p>
           Released: {this.props.film.release_year}
         </p>
+
         <p>
           Rating: {this.props.film.rating}
         </p>
+
         <p>
           Cast: {this.props.film.show_cast}
         </p>
+
+        <p>
+          Director: {this.props.film.director}
+        </p>
+
+        <p>
+          Runtime: {this.props.film.runtime}
+        </p>
+
       </div>
+      
       )
   }
 
