16a92a666d947d5dd72b8447ee3b17015a0ed76a
Robert Ball <robert.ball.1989@gmail.com>
2017-05-02 13:33:21 +0100

    can make a request to the api, getting films back

--- client/src/containers/ShowContainer.jsx
+++ client/src/containers/ShowContainer.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 class ShowContainer extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      films: [],
+      selectedFilm: null
+    }
+  }
+
+  componentDidMount(){
+    const url = "http://netflixroulette.net/api/api.php?actor=Nicolas%20Cage"
+    const request = new XMLHttpRequest()
+    request.open("GET", url)
+
+    request.onload = () => {
+      if(request.status === 200){
+        const jsonString = request.responseText
+        const films = JSON.parse(jsonString)
+        console.log(films)
+        this.setState( {films: films, selectedFilm: films[0]} )
+      }
+    }
+    request.send()
+  }
+
   render(){
     return(
       <p>This is the show container.</p>
